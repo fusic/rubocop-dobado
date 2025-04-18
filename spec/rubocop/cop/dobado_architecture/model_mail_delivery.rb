6cf376406869e139dc0184560dbe026ebc6dd8ba
Yuki Neba <94825279+ukey07@users.noreply.github.com>
2025-04-18 17:27:28 +0900

    feat: モデル内でのメール送信を検知するルールを追加

---
+++ spec/rubocop/cop/dobado_architecture/model_mail_delivery.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::DobadoArchitecture::ModelMailDelivery, :config do
+  example 'モデル内でのメール送信を検知' do
+    expect_offense(<<~RUBY)
+      class User < ApplicationRecord
+        after_create do
+          UserMailer.welcome_email(user).deliver_now
+                                         ^^^^^^^^^^^ モデル内でメール送信（deliver_now/deliver_later）を呼び出すのは非推奨です
+        end
+      end
+    RUBY
+  end
+
+  example '&.を使用したメール送信も検知' do
+    expect_offense(<<~RUBY)
+      class User < ApplicationRecord
+        after_create do
+          UserMailer.welcome_email(user)&.deliver_now
+                                          ^^^^^^^^^^^ モデル内でメール送信（deliver_now/deliver_later）を呼び出すのは非推奨です
+        end
+      end
+    RUBY
+  end
+end
